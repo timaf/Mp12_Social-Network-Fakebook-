296fe42e7eace6003a719858427f5e05da4fc12c
timaf <lo_sa_na@yahoo.com>
2018-04-04 11:41:08 +0200

    start

---
+++ src/main/java/at/refugeescode/Mp12_SocialNetworkFakebook/Mp12SocialNetworkFakebookApplication.java
@@ -0,0 +1,12 @@
+package at.refugeescode.Mp12_SocialNetworkFakebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mp12SocialNetworkFakebookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mp12SocialNetworkFakebookApplication.class, args);
+	}
+}
